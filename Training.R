6c2139af68787acf25d8de7d4f0c440ed0a2a3e2
JohnAitken <delgesu@gmail.com>
2022-06-22 14:59:03 +0100

    code created for first plot

--- Training.R
+++ Training.R
@@ -1,1 +1,14 @@
-library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+#load the data and group by provider count per dropoff level
+df <- read.csv("Final_Dropoff_Data_xl.csv") %>%
+    count(Count, wt=Count) %>%
+    rename(Providers = n,Dropoffs = Count)
+
+ plt <- ggplot(df, aes(x=Dropoffs,y=Providers)) +
+        geom_col() +
+        geom_text(aes(x = Dropoffs, y = Providers, label = Providers), stat = "sum", vjust = -0.5) +
+        scale_x_continuous(n.breaks = 10, limits = c(0,10))
+
+print(plt)
