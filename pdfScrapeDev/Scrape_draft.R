aaf41c09311af8835d64936772a42ad2307ffd66
JohnAitken <delgesu@gmail.com>
2023-11-10 18:54:48 +0000

    Initial dev done, 4 versions done

--- pdfScrapeDev/Scrape_draft.R
+++ pdfScrapeDev/Scrape_draft.R
@@ -1,10 +1,18 @@
-library("pdftools")
-library("tidyverse")
+# library("pdftools")
+# library("tidyverse")
+
+# setwd("C:/Users/JA/Documents/Git_Repositories/R/pdfScrapeDev")
+
+# scrape1 <- pdftools::pdf_text("10088075_10055_202324.pdf")
+
+# scrape2 <- strsplit(scrape1, "\n")
+
+# print(head(scrape2[[1]]))
+
+library(tesseract)
 
 setwd("C:/Users/JA/Documents/Git_Repositories/R/pdfScrapeDev")
 
-scrape1 <- pdftools::pdf_text("10088075_10055_202324.pdf")
-
-scrape2 <- strsplit(scrape1, "\n")
-
-print(head(scrape2[[1]]))
+eng <- tesseract("eng")
+text <- tesseract::ocr("10088075_10055_202324.pdf", engine = eng)
+cat(text)
